c0a1b22c058b580cc96f20f1555e583151daf7f3
venkyvajrala <59252985+venkyvajrala@users.noreply.github.com>
2020-07-01 13:22:59 +0530

    Update gradient.m

--- gradient.m
+++ gradient.m
@@ -3,7 +3,7 @@ function [theta,j_history] = gradient(X,y,theta,alpha,num_iterations)
     m=length(y);
     X=[ones(m,1),X];
     for i=1:num_iterations
-        theta=theta-((alpha/m)*(((X*theta)-y)'*X));
+         theta=theta-((alpha/m)*(((X*theta)-y)'*X))'
         
         j_history(i)=costfunction(X,y,theta);
         
