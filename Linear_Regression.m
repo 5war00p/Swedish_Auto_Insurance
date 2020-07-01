4f5e0f6c234767c83b7d66715b190efc6cca99b4
venkyvajrala <59252985+venkyvajrala@users.noreply.github.com>
2020-07-01 14:23:49 +0530

    Update Linear_Regression.m

--- Linear_Regression.m
+++ Linear_Regression.m
@@ -1,9 +1,18 @@
 [X,y,x1,y1]=data_load();
-plotData(X,y);
+%plotData(X,y);
 theta=zeros(size(X,2)+1,1);
-num_iterations=1000;
-alpha=0.001;
+num_iterations=100000;
+alpha=0.0001;
+%hold on;
 [theta,j_history]=gradient(X,y,theta,alpha,num_iterations);
-plotData(1:num_iteration,j_history); %it has to decrease for every iteration
+%plotData(1:num_iterations,j_history);
+%X=[ones(length(y),1),X];
+%plot(X(:,2), X*theta, '-')
+%hold off;
+x1=[ones(length(y1),1),x1];
+%plotData(x1,y1);
+%hold on;
+plot(x1, x1*theta, '-')
+
 
 
