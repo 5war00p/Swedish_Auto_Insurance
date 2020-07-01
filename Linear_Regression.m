f21409c2042997e376935fc476a895c2a68d57a0
venkyvajrala <59252985+venkyvajrala@users.noreply.github.com>
2020-07-01 15:03:33 +0530

    Update Linear_Regression.m

--- Linear_Regression.m
+++ Linear_Regression.m
@@ -1,18 +1,18 @@
-[X,y,x1,y1]=data_load();
-%plotData(X,y);
+data=load('first_project_data.txt');
+X=data(:,1);
+y=data(:,2);
+plotData(X,y);
+hold on;
 theta=zeros(size(X,2)+1,1);
 num_iterations=100000;
 alpha=0.0001;
-%hold on;
 [theta,j_history]=gradient(X,y,theta,alpha,num_iterations);
-%plotData(1:num_iterations,j_history);
-%X=[ones(length(y),1),X];
-%plot(X(:,2), X*theta, '-')
-%hold off;
-plotData(x1,y1);
-hold on;
-x1=[ones(length(y1),1),x1];
-plot(x1(:,2), x1*theta, '-')
+X=[ones(length(y),1),X];
+plot(X(:,2), X*theta, '-')
+
+
+
+
 
 
 
