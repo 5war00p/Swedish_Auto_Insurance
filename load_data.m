854ab7b323d6a28dd59510b0feaa3fa20dab11e0
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-01 12:41:02 +0530

    Update load_data.m

--- load_data.m
+++ load_data.m
@@ -1,6 +1,8 @@
-data = xlsread('Data.xlsx');
-
-X = data(:,1);
-y = data(:,2);
-
-%plot(X,y,'.r','MarkerSize',5);
+function [X,y] = load_data()
+    data = xlsread('Data.xlsx');
+    
+    X = data(:,1);
+    y = data(:,2);
+    
+    %plot(X,y,'.r','MarkerSize',5);
+end
