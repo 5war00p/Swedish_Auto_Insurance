120e5ab63aa5028349f6b49797f549b3e6dcdf11
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-01 19:57:15 +0530

    Update cost with grad

--- costfunction.m
+++ costfunction.m
@@ -1,5 +1,6 @@
-function [J] =costfunction(X,y,theta)
+function [J,grad] = costFunction(X,y,theta)
     m=length(X);
     h = X*theta;
     J= (1/(2*m)) * sum((h-y).^2);
+    grad = (1/m) .* X' * (h-y);
 end
